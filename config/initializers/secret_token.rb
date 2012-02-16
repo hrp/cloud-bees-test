1470bb688b965c163e0f400a94aaee654afc3eac
Hiten Parmar <hrparmar@gmail.com>
2012-02-15 18:03:51 -0800

    first commit

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+CloudBeesTest::Application.config.secret_token = '9c011ce36d3e0264ceee3672a4bfdcf2d382ec70810dda9bcab5391ce101a16ca8a387ec33249dd934619ec180c9306e6c9bda4036a352f72e1c61bedbebd875'
